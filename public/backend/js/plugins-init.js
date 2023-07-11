6ea80cf3deb50e269d6e2dc61d84c2e34a9e7d6f
shafi1997 <msh.shafiul@gmail.com>
2023-07-11 23:47:01 +0600

    format

--- public/backend/js/plugins-init.js
+++ public/backend/js/plugins-init.js
@@ -1,8 +1,8 @@
-$(function() {
+$(function () {
     $.ajaxSetup({
         headers: {
-            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
-        }
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
     });
     // $(".alert").alert();
     // window.setTimeout(function() {
@@ -28,18 +28,20 @@ $(function() {
     //         chk.prop('checked', false);
     //     }
     // });
-    $(".hasChildOptions").on('change', function() {
+    $(".hasChildOptions").on("change", function () {
         if ($(this).prop("checked") == true) {
-            if ($(this).data('show_child')) {
-                if ($(this).data('show_child') == 1) {
-                    $('#' + $(this).data('child_id')).show();
+            if ($(this).data("show_child")) {
+                if ($(this).data("show_child") == 1) {
+                    $("#" + $(this).data("child_id")).show();
                 } else {
-                    $('#' + $(this).data('child_id')).hide();
+                    $("#" + $(this).data("child_id")).hide();
                 }
-            } else $('#' + $(this).data('child_id')).show();
+            } else $("#" + $(this).data("child_id")).show();
         } else {
-            $('#' + $(this).data('child_id')).find('input').prop('checked', false);
-            $('#' + $(this).data('child_id')).hide();
+            $("#" + $(this).data("child_id"))
+                .find("input")
+                .prop("checked", false);
+            $("#" + $(this).data("child_id")).hide();
         }
     });
 });
@@ -71,185 +73,180 @@ function changeStatus(arg) {
         icon: "warning",
         buttons: true,
         dangerMode: true,
-    })
-        .then((willDelete) => {
-            if (willDelete) {
-                $.ajax({
-                    url: status.data('route'),
-                    type: 'PATCH',
-                    data: {
-                        status: status.data('value'),
-                    },
-                    success: res => {
-                        swal({
-                            icon: 'success',
-                            title: 'Success',
-                            text: res.message
-                        });
-                        $('.table').DataTable().ajax.reload();
-                    },
-                    error: err => {
-                        swal({
-                            icon: 'error',
-                            title: 'Oops...',
-                            text: err.responseJSON.message
-                        });
-                    }
-                });
-            }
-        });
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: status.data("route"),
+                type: "PATCH",
+                data: {
+                    status: status.data("value"),
+                },
+                success: (res) => {
+                    swal({
+                        icon: "success",
+                        title: "Success",
+                        text: res.message,
+                    });
+                    $(".table").DataTable().ajax.reload();
+                },
+                error: (err) => {
+                    swal({
+                        icon: "error",
+                        title: "Oops...",
+                        text: err.responseJSON.message,
+                    });
+                },
+            });
+        }
+    });
 }
-
 
 function ajaxDelete(arg, type) {
     let args = $(arg);
     swal({
-            title: "Are you sure?",
-            text: "This action will delete this record and cannot be undone!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-        .then((willDelete) => {
-            if (willDelete) {
-                $.ajax({
-                    url: args.data('route'),
-                    type: 'delete',
-                    data: {
-                        id: args.data('value'),
-                    },
-                    success: res => {
-                        swal({
-                            icon: 'success',
-                            title: 'Success',
-                            text: res.message
-                        }).then((confirm) => {
-                            if (confirm) {
-                                if (type == 'dt') {
-                                    $('.table').DataTable().ajax.reload();
-                                } else {
-                                    window.location.reload();
-                                }
+        title: "Are you sure?",
+        text: "This action will delete this record and cannot be undone!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: args.data("route"),
+                type: "delete",
+                data: {
+                    id: args.data("value"),
+                },
+                success: (res) => {
+                    swal({
+                        icon: "success",
+                        title: "Success",
+                        text: res.message,
+                    }).then((confirm) => {
+                        if (confirm) {
+                            if (type == "dt") {
+                                $(".table").DataTable().ajax.reload();
+                            } else {
+                                window.location.reload();
                             }
-                        });
-                    },
-                    error: err => {
-                        swal({
-                            icon: 'error',
-                            title: 'Oops...',
-                            text: err.responseJSON.message
-                        });
-                    }
-                });
-            }
-        });
+                        }
+                    });
+                },
+                error: (err) => {
+                    swal({
+                        icon: "error",
+                        title: "Oops...",
+                        text: err.responseJSON.message,
+                    });
+                },
+            });
+        }
+    });
 }
 
 function ajaxAllDelete(arg, type) {
     let args = $(arg);
     var del_ids = [];
     // Read all checked checkboxes
-    $("input:checkbox[class=check]:checked").each(function() {
+    $("input:checkbox[class=check]:checked").each(function () {
         del_ids.push($(this).val());
     });
     // Check checkbox checked or not
     if (del_ids.length > 0) {
         swal({
-                title: "Are you sure?",
-                text: "This action will delete all records and cannot be undone!",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            })
-            .then((willDelete) => {
-                if (willDelete) {
-                    $.ajax({
-                        url: args.data('route'),
-                        type: 'delete',
-                        data: {
-                            ids: del_ids,
-                        },
-                        success: res => {
-                            swal({
-                                icon: 'success',
-                                title: 'Success',
-                                text: res.message
-                            }).then((confirm) => {
-                                if (confirm) {
-                                    if (type == 'dt') {
-                                        $('.table').DataTable().ajax.reload();
-                                    } else {
-                                        window.location.reload();
-                                    }
+            title: "Are you sure?",
+            text: "This action will delete all records and cannot be undone!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    url: args.data("route"),
+                    type: "delete",
+                    data: {
+                        ids: del_ids,
+                    },
+                    success: (res) => {
+                        swal({
+                            icon: "success",
+                            title: "Success",
+                            text: res.message,
+                        }).then((confirm) => {
+                            if (confirm) {
+                                if (type == "dt") {
+                                    $(".table").DataTable().ajax.reload();
+                                } else {
+                                    window.location.reload();
                                 }
-                            });
-                        },
-                        error: err => {
-                            swal({
-                                icon: 'error',
-                                title: 'Oops...',
-                                text: err.responseJSON.message
-                            });
-                        }
-                    });
-                }
-            });
+                            }
+                        });
+                    },
+                    error: (err) => {
+                        swal({
+                            icon: "error",
+                            title: "Oops...",
+                            text: err.responseJSON.message,
+                        });
+                    },
+                });
+            }
+        });
     }
 }
-
 
 function ajaxEdit(arg, type) {
     let args = $(arg);
     $.ajax({
-        url: args.data('route'),
-        type: 'get',
+        url: args.data("route"),
+        type: "get",
         data: {
-            id: args.data('value'),
+            id: args.data("value"),
         },
-        success: res => {
+        success: (res) => {
             $("#ajax_modal_container").html(res.modal);
-            $("#editModal").modal('show');
+            $("#editModal").modal("show");
         },
-        error: err => {
+        error: (err) => {
             swal({
-                icon: 'error',
-                title: 'Oops...',
-                text: err.responseJSON.message
+                icon: "error",
+                title: "Oops...",
+                text: err.responseJSON.message,
             });
-        }
+        },
     });
-};
+}
 
 function ajaxStore(e, form, modal) {
     e.preventDefault();
     // let formData = $(form).serialize();
     let formData = new FormData(form);
     $.ajax({
-        url: $(form).attr('action'),
-        type: 'POST',
+        url: $(form).attr("action"),
+        type: "POST",
         data: formData,
         contentType: false,
         processData: false,
-        success: res => {
+        success: (res) => {
             swal({
-                icon: 'success',
-                title: 'Success',
-                text: res.message
+                icon: "success",
+                title: "Success",
+                text: res.message,
             }).then((confirm) => {
                 if (confirm) {
-                    $('.table').DataTable().ajax.reload();
-                    $("#" + modal).modal('hide');
+                    $(".table").DataTable().ajax.reload();
+                    $("#" + modal).modal("hide");
                     $(form).trigger("reset");
                 }
             });
         },
-        error: err => {
+        error: (err) => {
             swal({
-                icon: 'error',
-                title: 'Oops...',
-                text: err.responseJSON.message
+                icon: "error",
+                title: "Oops...",
+                text: err.responseJSON.message,
             });
-        }
+        },
     });
 }
 
